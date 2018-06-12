7eada1e94e8ca740f6621adfe178582117ccc942
kailinc <kailinc@bu.edu>
2018-06-12 16:23:35 +0700

    Finished reg exercise on classification - wrote code to classify microchip with multiple features - implemented regularization

--- ml-standford/machine-learning-ex2/ex2/costFunctionReg.m
+++ ml-standford/machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,6 +17,17 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+
+regCost = sum(theta(2:end) .^ 2) * lambda / (2 * m);
+
+J = (((-y)' * log(h)) - ((1 - y)' * log(1 - h)))/m + regCost;
+
+thetaCopy = theta;
+thetaCopy(1) = 0;
+
+regGrad = (thetaCopy * lambda)/m;
+grad = (X'*(h - y))/m + regGrad;
 
 
 
