385d53aee600fdda3ebbe0fb993718e3232b5bc7
kailinc <kailinc@bu.edu>
2018-06-21 15:01:34 +0700

    Added comments for dataset3Params Comfort: 5 Clarity: 5 Added comments to dataset3Params file. This is to make it more clear what each variable is for.

--- ml-standford/machine-learning-ex6/ex6/dataset3Params.m
+++ ml-standford/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,18 +22,38 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+% values for C
 C_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+
+% values for sigma
 sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+
+% initialize arbitary error number for checking cv error
 minError = 1000;
 
+% loop vector of C values
 for c = 1:length(C_vec),
+
+  % get cur C value
   curC = C_vec(c);
+
+  % loop vector of Sigma values
   for s = 1:length(sigma_vec),
+    % get cur sigma value
     curS = sigma_vec(s);
+
+    % get a svm + gaussiankernel model
     model = svmTrain(X, y, curC, @(x1, x2) gaussianKernel(x1, x2, curS));
+
+    % get predictions of the model of the Xvals
     predictions = svmPredict(model, Xval);
+
+    % error of the model
     error = mean(double(predictions ~= yval));
+
+    % if we get lower error than the min, set c, sigma, minError to the current values
     if (error < minError),
+      % just for us to know the alg is working correctly 
       fprintf('\nPrediction error is %f with C %f and sigma %f', error, curC, curS);
       minError = error;
       C = curC;
